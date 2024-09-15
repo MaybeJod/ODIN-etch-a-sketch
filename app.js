2b69ad066de4f236bc3cb535aa190f926a4d2208
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-09-15 13:12:06 +0200

    Step 4: added a reset button and a slider for grid
    Add a button that will reset grid. Slider which will adjust the grid size.
    asking for the number of squares per side for the new grid. Once entered.

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-// step 2
-/* Create a webpage with a 16x16 grid of square divs.
-    - Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file!
-
-    - It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file.
-
-    - Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. This project is an opportunity specifically to practice Flexbox!
-    
-    - Be careful with borders and margins, as they can adjust the size of the squares!
- */
-
 // variables
-const ink = ["#000000"];
-
 const grid = document.querySelector(".grid");
+const resetBtn = document.querySelector("#reset");
 
+// slider for grid size
+let slider = document.querySelector("#sliderValue");
+let output = document.querySelector("#displaySliderValue");
+output.innerHTML = slider.value;
 
-// -------------------------------------------------------------------------------------
+// get value from slider to use for createGrid
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    resetGrid();
+    changeGridSize();
+}
 
-// functions 
+createGrid(slider.value);
+
 function createGrid(gridSize) {
     //creates the grid with gridSize parameter
     grid.style.cssText =
@@ -29,53 +27,40 @@ function createGrid(gridSize) {
     // square the grid
     const numberOfCells = gridSize ** 2;
     
-    // creates the cells with div, as a child of .grid
+    // creates the cells with a div, as a child of .grid
     for(i = 0; i < numberOfCells; i++) {
         const gridCell = document.createElement("div");
         gridCell.classList.add("cell");
         grid.appendChild(gridCell);
-
+        // hover effect so there is a color trail after the mouse 
         gridCell.addEventListener("mouseover", colorTrail);
     };
-    
+
 }
 
+// random colors in HSL for color trail 
+function getRandomColor() {
+    color = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+    return color;
+}
 
-// Kevin was here 
-
-
-
+// color trail for mouseover
 function colorTrail() { 
-    this.style.backgroundColor = ink[0];
+    this.style.backgroundColor = getRandomColor();
+    this.style.border = "none";
 }
 
+// reset the gird 
+function resetGrid() {
+    grid.innerHTML = "";
+    createGrid(slider.value);
+}
 
-createGrid(100);
+// event listener for reset button
+resetBtn.addEventListener("click", resetGrid)
 
-
-// -------------------------------------------------------------------------------------
-
-// step 3
-/* Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-
-    - Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
-
-    - There are multiple ways to change the color of the divs, including:
-        - Adding a new class to the div.
-        - Changing the div’s background color using JavaScript. */
-
-    
-
-
-
-// -------------------------------------------------------------------------------------
-
-// step 4
-/* Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
-    - Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
-
-    - Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-
-    - Also check out prompts.
-
-    - You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used. */
+// when slider gets a new value, change the grid size. 
+function changeGridSize() {
+    resetGrid();
+    createGrid(slider.value);
+}
